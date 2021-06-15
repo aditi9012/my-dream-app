7196fb7c18adea6578ff3a33b9236c2fc58639c2
aditi9012 <aditi.1721it1011@kiet.edu>
2021-06-15 23:45:16 +0530

    added 2-way data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,22 @@
-import { Component } from "@angular/core";
+import { Component} from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    
 })
+
 export class ServerComponent{
   serverId:number=5;
   serverStatus:string='offline';
+  eventValue='';
+  eventValueTwoWayBind='Hello Jack';
   
+  onUpdateServerName(event:any){
+      this.eventValue=(<HTMLInputElement>event.target).value;
+      //this.eventValueTwoWayBind=(<HTMLInputElement>event.target).value;
+
+  }
 }
