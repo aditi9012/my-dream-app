ba877e1c6668b7444324f1b7385644e16871c710
aditi9012 <aditi.1721it1011@kiet.edu>
2021-06-16 00:34:39 +0530

    added assignment 2 on data binding

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  updateUsername='';
+  buttonClicked=false;
+  onClick(){
+    
+      this.updateUsername='';
+    
+  }
+}
